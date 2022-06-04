fb4dee1be1203dde3382030b1d8ad0c3a1afcb79
Yogesh Jadhav <pgyogesh@outlook.com>
2022-06-04 14:24:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -279,7 +279,7 @@ class Settings:
         "SLUGIFY": "y",
         "HOME_GRAPH": "y",
         "PAGE_GRAPH": "y",
-        "SUBSECTION_SYMBOL": "👉",
+        "SUBSECTION_SYMBOL": "➤",
         "LOCAL_GRAPH": "",
         "GRAPH_LINK_REPLACE": "",
         "STRICT_LINE_BREAKS": "y",
